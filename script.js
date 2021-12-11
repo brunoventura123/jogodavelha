ff86592f1c7a60d4168305e9bdfe8453ad89577b
Bruno Leonardo Sancho Ventura <86660522+brunoventura123@users.noreply.github.com>
2021-12-11 15:39:29 -0300

    a1,b1,c1 add

--- script.js
+++ script.js
@@ -1,103 +1,109 @@
 //dados iniciais
 let square = {
-    a1: '', a2: '', a3: '',
-    b1: '', b2: '', b3: '',
-    c1: '', c2: '', c3: ''
-}
-let player = ''
-let warning = ''
-let playing = false
+  a1: "",
+  a2: "",
+  a3: "",
+  b1: "",
+  b2: "",
+  b3: "",
+  c1: "",
+  c2: "",
+  c3: "",
+};
+let player = "";
+let warning = "";
+let playing = false;
 
-reset()
+reset();
 //eventos
-document.querySelector('.reset').addEventListener('click', reset)
-document.querySelectorAll('.item').forEach(item =>{
-    item.addEventListener('click', itemClick)
-})
+document.querySelector(".reset").addEventListener("click", reset);
+document.querySelectorAll(".item").forEach((item) => {
+  item.addEventListener("click", itemClick);
+});
 //Funções
-function itemClick(event){
-    let item = event.target.getAttribute('data-item')
-    if(playing && square[item] === '') {
-        square[item] = player
-        renderSquare()
-        togglePlayer()
-    }
+function itemClick(event) {
+  let item = event.target.getAttribute("data-item");
+  if (playing && square[item] === "") {
+    square[item] = player;
+    renderSquare();
+    togglePlayer();
+  }
 }
-function reset(){
-    warning = ''
-    let random = Math.floor(Math.random() *2)
-    player = (random === 0) ? 'x': 'o'
+function reset() {
+  warning = "";
+  let random = Math.floor(Math.random() * 2);
+  player = random === 0 ? "x" : "o";
 
-    for(let i in square) {
-        square[i] = ''
-    }
+  for (let i in square) {
+    square[i] = "";
+  }
 
-    playing = true
-    
-    renderSquare()
-    renderInfo()
-}
+  playing = true;
 
-function renderSquare(){
-    for(let i in square) {
-        let item = document.querySelector(`div[data-item=${i}]`)
-        item.innerHTML = square[i]
-    }
-    checkGame()
+  renderSquare();
+  renderInfo();
 }
 
-function renderInfo(){
-    document.querySelector('.vez').innerHTML = player
-    document.querySelector('.resultado').innerHTML = warning
+function renderSquare() {
+  for (let i in square) {
+    let item = document.querySelector(`div[data-item=${i}]`);
+    item.innerHTML = square[i];
+  }
+  checkGame();
 }
 
-function togglePlayer(){
-    player = (player === 'x') ? 'o': 'x'
-    renderInfo()
+function renderInfo() {
+  document.querySelector(".vez").innerHTML = player;
+  document.querySelector(".resultado").innerHTML = warning;
 }
 
-function checkGame(){
-    if(checkWinnerFor('x')) {
-        warning = 'Bruno venceu!'
-        playing = false
-    } else if(checkWinnerFor('o')){
-        warning = 'Ketlin venceu!'
-        playing = false
-    } else if(isFull()) {
-        warning = 'Deu empate!'
-        playing = false
-    }
+function togglePlayer() {
+  player = player === "x" ? "o" : "x";
+  renderInfo();
+}
+
+function checkGame() {
+  if (checkWinnerFor("x")) {
+    warning = "Bruno venceu!";
+    playing = false;
+  } else if (checkWinnerFor("o")) {
+    warning = "Ketlin venceu!";
+    playing = false;
+  } else if (isFull()) {
+    warning = "Deu empate!";
+    playing = false;
+  }
 }
 
 function checkWinnerFor(player) {
-    let pos = [
-        'a1,a2,a3',
-        'b1,b2,b3',
-        'c1,c2,c3',
+  let pos = [
+    "a1,a2,a3",
+    "b1,b2,b3",
+    "c1,c2,c3",
 
-        'a1,b1,b2',
-        'a2,b2,c2',
-        'a3,b3,c3',
+    "a1,b1,c1",
+    "a2,b2,c2",
+    "a3,b3,c3",
 
-        'a1,b2,c3',
-        'a3,b2,c1'
-    ]
+    "a1,b2,c3",
+    "a3,b2,c1",
+  ];
 
-    for(let w in pos) {
-        let pArray = pos[w].split(',')
-        let hasWon = pArray.every(option => square[option] === player)
-        if(hasWon) {
-            return true
-        }
+  for (let w in pos) {
+    let pArray = pos[w].split(",");
+    let hasWon = pArray.every((option) => square[option] === player);
+    if (hasWon) {
+      return true;
     }
-    return false
+  }
+  return false;
 }
 
-function isFull(){
-    for(let i in square) {
-        if(square[i] === '') {
-            return false
-        }
+function isFull() {
+  for (let i in square) {
+    if (square[i] === "") {
+      return false;
     }
-    return true
+  }
+  return true;
 }
